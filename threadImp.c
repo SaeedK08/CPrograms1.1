557c9db2683b5200dba87492ed30f75be077ba0c
Saeed Kassab <saeedkassab2000@gmail.com>
2025-05-28 11:33:00 +0200

    Add a test for concurrency and pthread_join().

--- threadImp.c
+++ threadImp.c
@@ -1,23 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <unistd.h>
 #define BSIZE 16
 #define NTHREADS 4 
 
 typedef struct {
     unsigned char *buffer;
+    int thread_id;
     int length;
 } Buffer;
 
 void *doClear(void *vargp)
 {
+    // int counter = 0;
     Buffer *piece = (Buffer *) vargp;
-    printf("Working on %p\n" ,piece);
-    for (int i = 0; i < piece->length; i++)
-    {
-        *(piece->buffer) = '*';
-        piece->buffer ++;
-    }
+        printf("Working on %p\n" ,piece);
+        for (int i = 0; i < piece->length; i++)
+        {
+            *(piece->buffer) = '*';
+            piece->buffer ++;
+        }
+    // while (1)
+    // {
+    //     // sleep(1);
+    //     // printf("thread %d is working\n" ,(piece->thread_id));
+    //     // counter ++;
+    //     // if (counter >= 5) break;
+        
+    // }
+    return (void*) vargp;
 }
 
 void clearBuffer(unsigned char *p, int length)
@@ -28,7 +40,9 @@ void clearBuffer(unsigned char *p, int length)
     {
         pieces[i].buffer = p + i * (length / 4);
         pieces[i].length = length / 4;
+        pieces[i].thread_id = i;
         pthread_create(&wt[i], NULL, doClear, (void *)&pieces[i]);
+        // pthread_join(wt[i], NULL);
     }
     
     for (int i = 0; i < NTHREADS; i++)
